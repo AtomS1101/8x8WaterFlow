867bb615595fafede42d7eddec8bc91ca386478a
Atom <121838254+AtomS1101@users.noreply.github.com>
2025-09-22 16:49:47 +0900

    Add files via upload

---
+++ simulation/main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from matrix import Matrix
+import time
+from random import randint
+
+def main():
+	matrix = Matrix()
+	matrix.setArray()
+	matrix.show()
+	angle = 45
+
+	while (True):
+		for _ in range(2):
+			print(angle)
+			# if randint(0, 10) == 1:
+			# 	matrix.array[1][0][0] = True
+			matrix.refresh(angle)
+			matrix.shift()
+			matrix.show()
+			time.sleep(0.05)
+			# time.sleep(0.07)
+		angle += 2
+		if angle >= 360:
+			angle = 0
+
+if __name__ == "__main__":
+	main()
